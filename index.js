44573e347250037d30eebaabca041380fa5df995
Shahid <abdul.baset@aci-bd.com>
2020-11-04 17:12:34 +0600

    install winson logger and pass correlation id

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 
+const processRequest =  async (req, res, next) => {
+    let correlationId = req.headers['x-correlation-id'];
+    if(!correlationId) {
+        correlationId = Date.now().toString();
+        req.headers['x-correlation-id'] = correlationId;
+    }
+    res.set('x-correlation-id', correlationId);
+
+    return next();
+}
+
+app.use(processRequest);
+
 connectToDb();
 configure(app);
 
