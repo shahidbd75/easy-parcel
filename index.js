6dc74593ea8e074a58bbddde7b06fab3deaf4acc
Shahid <abdul.baset@aci-bd.com>
2020-11-05 12:34:34 +0600

    Add Console and file debug logger

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import connectToDb from './mongo';
 import configure from "./controllers";
-import  { handleErrors }  from "./middlewares/handleErrors";
+import { handleErrors }  from "./middlewares/handleErrors";
+import winston from 'winston';
+import expressWinston from 'express-winston';
+import winstonFile from 'winston-daily-rotate-file';
+import winstonMongo from 'winston-mongodb';
+import { ElasticsearchTransport } from 'winston-elasticsearch';
 
 const app = express();
 const port = 3000;
@@ -21,6 +26,32 @@ const processRequest =  async (req, res, next) => {
 app.use(processRequest);
 
 connectToDb();
+
+const getMessage = (req, res) => {
+    let obj = {
+        correlationId: req.headers['x-correlation-id'],
+        requestBody: req.body
+    };
+    return JSON.stringify(obj);
+}
+
+const fileInfoTransport = new (winston.transports.DailyRotateFile)({
+    filename: 'log-info-%DATE%.log',
+    datePattern: 'yyyy-MM-DD-HH'
+});
+
+const infoLogger = expressWinston.logger({
+    transports: [
+        new winston.transports.Console(),
+        fileInfoTransport
+    ],
+    format: winston.format.combine(winston.format.colorize(),winston.format.json()),
+    meta:false,
+    msg: getMessage
+});
+
+app.use(infoLogger);
+
 configure(app);
 
 app.use(handleErrors);
