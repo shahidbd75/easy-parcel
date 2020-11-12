647011e0e44776e02cf5035dabfa23f418f14edf
Shahid <abdul.baset@aci-bd.com>
2020-11-12 17:23:03 +0600

    add babel config

--- mongo.js
+++ mongo.js
@@ -7,10 +7,7 @@ const options = {};
 export const connectToDb = () => {
     mongoose.connect(uri, options,(err) => {
         if(err) {
-            console.error(err);
-        }
-        else {
-            console.log("database connection established");
+            throw err;
         }
     })
 }
